69ae82fb7fa105f0e869b26a96275955b60bc141
Shubham Acharya <104817428+shubham190398@users.noreply.github.com>
2023-07-25 13:20:13 +0530

    Deleted self.tags = []. This was a redundant declaration as the parent class constructor already does so

--- cards/cards.py
+++ cards/cards.py
@@ -17,14 +17,12 @@ class AttackCard(Card):
 
     def __init__(self, name='Attack', desc='This is a dummy attack card.', cost=0):
         super().__init__(name, desc, cost)
-        self.tags = []
 
 
 class SkillCard(Card):
 
     def __init__(self, name='Skill', desc='This is a dummy skill card.', cost=0):
         super().__init__(name, desc, cost)
-        self.tags = []
 
 
 
